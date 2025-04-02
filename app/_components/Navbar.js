a175de3394285cebcceeb8c65504f4d13302c6af
gautamp11 <gautam1564p1@gmail.com>
2025-04-02 16:28:53 +0530

    updated logo

--- app/_components/Navbar.js
+++ app/_components/Navbar.js
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
     <nav className="navbar flex justify-center md:justify-between items-center px-6 py-4 text-secondary font-semibold">
       {/* <h1 className="text-2xl font-bold">Herbanya</h1> */}
-      <img src="/logo.png" alt="brand-logo" className="h-18 w-auto" />
+      <img src="/logo-new.png" alt="brand-logo" className="h-18 w-auto" />
       <div className="md:nav-links md:flex md:gap-5 hidden ">
         <Link
           className="hover:bg-secondary hover:text-white  transition duration-300 rounded-lg px-3 py-1"
